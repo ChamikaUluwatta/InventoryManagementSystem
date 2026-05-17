a9eda53da3d2b50bab44c3fad5116b3260b242e2
uc <u.chamikao2001@gmail.com>
2026-05-17 23:08:38 +0530

    migration issue fixed

--- AuthService/src/main/resources/db/migration/V1__create_auth_tables.sql
+++ AuthService/src/main/resources/db/migration/V1__create_auth_tables.sql
@@ -1,6 +1,5 @@
 CREATE TABLE users (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name       VARCHAR(255),
     password   VARCHAR(255) NOT NULL,
     email      VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
