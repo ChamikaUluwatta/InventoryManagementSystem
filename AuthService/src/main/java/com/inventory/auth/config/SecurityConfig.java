bc4d392a7d7a18b5086be2fa917b8bac18f71d0a
uc <u.chamikao2001@gmail.com>
2026-05-29 14:26:28 +0530

    Add JWT RSA key configuration and Docker setup for auth service

--- AuthService/src/main/java/com/inventory/auth/config/SecurityConfig.java
+++ AuthService/src/main/java/com/inventory/auth/config/SecurityConfig.java
@@ -1,10 +1,5 @@
 package com.inventory.auth.config;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,27 +31,5 @@ public class SecurityConfig {
                 .anyRequest().authenticated());
 
         return http.build();
-    }
-
-    // TODO: Persist RSA keys securely
-    @Bean
-    public KeyPair rsaKeyPair() {
-        try {
-            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-            generator.initialize(2048);
-            return generator.generateKeyPair();
-        } catch (Exception e) {
-            throw new IllegalStateException("Failed to generate RSA key pair", e);
-        }
-    }
-
-    @Bean
-    public RSAPublicKey rsaPublicKey(KeyPair keyPair) {
-        return (RSAPublicKey) keyPair.getPublic();
-    }
-
-    @Bean
-    public RSAPrivateKey rsaPrivateKey(KeyPair keyPair) {
-        return (RSAPrivateKey) keyPair.getPrivate();
     }
 }
